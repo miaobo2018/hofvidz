d2dbc7e57e93a5044d3e8f7f1329840a747570d4
Bo Miao <bo.miao@sjsu.edu>
2019-09-30 17:03:27 -0700

    debug11

--- hofvidz/settings.py
+++ hofvidz/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'hofvidz.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(),
+    'default': dj_database_url.config()
 }
 
 
